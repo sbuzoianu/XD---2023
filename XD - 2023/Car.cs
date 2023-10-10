5cb9c88ed94fd5ac75bfbc28eff8958d70d59493
Stefan Buzoianu <stefan.buzoianu@napa.fi>
2023-10-10 20:32:24 +0300

    Add project files.

---
+++ XD - 2023/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XD___2023 {
+        class Car {
+            string color = "red";
+
+            static void Main(string[] args) {
+                Car myObj1 = new Car();
+                Car myObj2 = new Car();
+                Console.WriteLine(myObj1.color);
+                Console.WriteLine(myObj2.color);
+            }
+        }
+}
