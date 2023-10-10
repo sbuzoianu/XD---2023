da63cde64f74a7e58a0f02fb76f7c44565be6b3d
Stefan Buzoianu <stefan.buzoianu@napa.fi>
2023-10-10 21:28:17 +0300

    Create a separate model class Car

--- XD - 2023/Car.cs
+++ XD - 2023/Car.cs
@@ -5,14 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace XD___2023 {
-        class Car {
-            string color = "red";
+    public class Car {
+        public string color = "red";
 
-            static void Main(string[] args) {
-                Car myObj1 = new Car();
-                Car myObj2 = new Car();
-                Console.WriteLine(myObj1.color);
-                Console.WriteLine(myObj2.color);
-            }
-        }
+    }
 }
---
+++ XD - 2023/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XD___2023 {
+    class Program {
+
+        static void Main(string[] args) {
+            Car myObj1 = new Car();
+            Car myObj2 = new Car();
+            Console.WriteLine(myObj1.color);
+            Console.WriteLine(myObj2.color);
+            Console.ReadKey();
+        }
+    }
+}
